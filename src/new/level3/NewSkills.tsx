b3e91f28585ca610208114be20ac05c0f8e6b5f7
Leewonno <dldnjssh123@naver.com>
2024-12-16 23:04:19 +0900

    [Feat] 스킬 정리

--- src/new/level3/NewSkills.tsx
+++ src/new/level3/NewSkills.tsx
@@ -27,15 +27,15 @@ export default function NewSkills() {
             <div className={style.skillCategoryBox}>
               <div className={style.skillCategoryTitle}>Frontend</div>
               <div className={style.skillImgBox}>
-                <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=HTML5&logoColor=white" alt="HTML" className={style.skillImg} />
-                <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat&logo=CSS3&logoColor=white" alt="CSS" className={style.skillImg} />
                 <img src="https://img.shields.io/badge/React-61DAFB?style=flat&logo=React&logoColor=white" alt="React" className={style.skillImg} />
-                <img src="https://img.shields.io/badge/Next.js-000000?style=flat&logo=nextdotjs&logoColor=white" alt="Next" className={style.skillImg} />
-                <img src="https://img.shields.io/badge/Vite-646CFF?style=flat&logo=vite&logoColor=white" alt="vite" className={style.skillImg} />
+                <img src="https://img.shields.io/badge/Next.js-000000?style=flat&logo=nextdotjs&logoColor=white" alt="Next" className={style.skillImg} />          
+                {/* <img src="https://img.shields.io/badge/Vite-646CFF?style=flat&logo=vite&logoColor=white" alt="vite" className={style.skillImg} /> */}
                 <img src="https://img.shields.io/badge/Redux-764ABC?style=flat&logo=redux&logoColor=white" alt="redux" className={style.skillImg} />
-                <img src="https://img.shields.io/badge/React Router-CA4245?style=flat&logo=reactrouter&logoColor=white" alt="reactrouter" className={style.skillImg} />
+                {/* <img src="https://img.shields.io/badge/React Router-CA4245?style=flat&logo=reactrouter&logoColor=white" alt="reactrouter" className={style.skillImg} /> */}
                 <img src="https://img.shields.io/badge/Recoil-3578E5?style=flat&logo=recoil&logoColor=white" alt="recoil" className={style.skillImg} />
                 <img src="https://img.shields.io/badge/Jquery-0769AD?style=flat&logo=jquery&logoColor=white" alt="jquery" className={style.skillImg} />
+                <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=HTML5&logoColor=white" alt="HTML" className={style.skillImg} />
+                <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat&logo=CSS3&logoColor=white" alt="CSS" className={style.skillImg} />  
               </div>
             </div>
             
@@ -46,7 +46,7 @@ export default function NewSkills() {
               <div className={style.skillImgBox}>
                 <img src="https://img.shields.io/badge/Node.js-339933?style=flat&logo=nodedotjs&logoColor=white" alt="Nodejs" className={style.skillImg} />
                 <img src="https://img.shields.io/badge/NestJs-E0234E?style=flat&logo=nestjs&logoColor=white" alt="NestJs" className={style.skillImg} />
-                <img src="https://img.shields.io/badge/springboot-6DB33F?style=flat&logo=springboot&logoColor=white" alt="springboot" className={style.skillImg} />
+                {/* <img src="https://img.shields.io/badge/springboot-6DB33F?style=flat&logo=springboot&logoColor=white" alt="springboot" className={style.skillImg} /> */}
                 <img src="https://img.shields.io/badge/Django-092E20?style=flat&logo=django&logoColor=white" alt="springboot" className={style.skillImg} />
               </div>
             </div>
@@ -54,7 +54,7 @@ export default function NewSkills() {
               <div className={style.skillCategoryTitle}>Database</div>
               <div className={style.skillImgBox}>
                 <img src="https://img.shields.io/badge/Mysql-4479A1?style=flat&logo=mysql&logoColor=white" alt="mysql" className={style.skillImg} />
-                <img src="https://img.shields.io/badge/Oracle SQL-F80000?style=flat&logo=oracle&logoColor=white" alt="oracle" className={style.skillImg} />
+                {/* <img src="https://img.shields.io/badge/Oracle SQL-F80000?style=flat&logo=oracle&logoColor=white" alt="oracle" className={style.skillImg} /> */}
                 <img src="https://img.shields.io/badge/PostgreSQL-4169E1?style=flat&logo=postgresql&logoColor=white" alt="PostgreSQL" className={style.skillImg} />
                 <img src="https://img.shields.io/badge/Firebase-FFCA28?style=flat&logo=firebase&logoColor=white" alt="firebase" className={style.skillImg} />
               </div>
