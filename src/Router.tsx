c63b3ae2439101d64d2e313d7e80aea7e6186144
Leewonno <dldnjssh123@naver.com>
2025-05-17 21:44:51 +0900

    [Feat] ver2 division

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Main from "./level3/Main";
-import NewMain from "./new/NewMain";
+import NewMain from "./ver2/NewMain";
 import My from "./my/My";
 
 const router = createBrowserRouter([
