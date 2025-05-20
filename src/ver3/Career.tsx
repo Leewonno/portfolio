094b182c3847fddbd1d8c2eb5e3fc2f7c81126f4
Leewonno <dldnjssh123@naver.com>
2025-05-20 23:27:54 +0900

    [Feat] career

--- src/ver3/Career.tsx
+++ src/ver3/Career.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components"
 import bg from "../img/nmixx.png"
+import seoulsoft from "../img/new/seoulsoft.png"
 import useObserver from "./lib/hook/useObserver"
 import { media } from "./lib/styles/media"
 import { motion } from "framer-motion"
-import { customChildVariants } from "./lib/styles/animation"
+import { customChildVariants, customVariants } from "./lib/styles/animation"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faFolderOpen } from "@fortawesome/free-solid-svg-icons"
+import { faFolderOpen, faPeopleGroup } from "@fortawesome/free-solid-svg-icons"
 
 const Section = styled.section`
   width: 100%;
@@ -57,30 +58,60 @@ const Title = styled.div`
   `}
 `
 
-const BottomBox = styled.div`
+const BottomBox = styled(motion.div)`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 1100px;
 `
 
-const Item = styled.div`
+const Item = styled(motion.div)`
   width: 100%;
   padding: 20px;
   background-color: #fff;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
+  gap: 8px;
+`
+const ItemLogo = styled.img`
+  width: 75px;
+  filter: brightness(0);
+  margin-bottom: 5px;
+`
+
+const ItemTitleBox = styled.div`
+  display: flex;
+  align-items: flex-end;
   gap: 10px;
 `
 
-const ItemDate = styled.div`
-  font-size: 14px;
+const ItemTitle = styled.div`
+  font-size: 25px;
+  font-family: 'S-CoreDream-7EXTRA_BOLD';
 `
 
-const ItemTitle = styled.div`
-  font-size: 20px;
-  font-family: 'S-CoreDream-7EXTRA_BOLD';
+const ItemDate = styled.div`
+  font-size: 15px;
+`
+
+const ItemDepartment = styled.div`
+  font-size: 15px;
+`
+
+const ItemContent = styled.div`
+  margin-top: 5px;
+  font-size: 16px;
+`
+
+const ItemSkillBox = styled.div`
+  display: flex;
+  gap: 5px;
+  margin-top: 20px;
+`
+
+const ItemSkill = styled.img`
+  height: 25px;
 `
 
 // 경력
@@ -96,10 +127,27 @@ export default function Career() {
           </Title>
         </TitleBox>
       </TopBox>
-      <BottomBox>
-        <Item>
-          <ItemDate>2024.04 ~ </ItemDate>
-          <ItemTitle>서울소프트</ItemTitle>
+      <BottomBox ref={ref} animate={animation} variants={customVariants}>
+        <Item variants={customChildVariants}>
+          <ItemLogo src={seoulsoft} />
+          <ItemTitleBox>
+            <ItemTitle>서울소프트</ItemTitle>
+            <ItemDate>2024.04 ~ (재직중)</ItemDate>
+          </ItemTitleBox>
+          <ItemDepartment>
+            <FontAwesomeIcon icon={faPeopleGroup} /> 모빌리티 개발팀 • 풀스택 개발
+          </ItemDepartment>
+          <ItemContent>
+            • 운수사 ERP 서비스 개발 <br />
+            • 운수사 ERP 서비스 개발 <br />
+            • 운수사 ERP 서비스 개발 <br />
+            • 운수사 ERP 서비스 개발 <br />
+            • 운수사 ERP 서비스 개발 <br />
+          </ItemContent>
+          <ItemSkillBox>
+            <ItemSkill src="https://img.shields.io/badge/Django-092E20?style=flat&logo=django&logoColor=white" alt="Django" />
+            <ItemSkill src="https://img.shields.io/badge/Jquery-0769AD?style=flat&logo=jquery&logoColor=white" alt="jquery" />
+          </ItemSkillBox>
         </Item>
       </BottomBox>
     </Section>
